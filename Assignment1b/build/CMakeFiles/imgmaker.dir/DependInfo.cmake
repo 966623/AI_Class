238c4a1bcccd5c51ba041c6701cb35f8fdf9e079
Sean Lin <linx0486@umn.edu>
2016-09-28 21:56:27 -0500

    shading

---
+++ Assignment1b/build/CMakeFiles/imgmaker.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/Sean/Documents/CSCI5607/Assignment1b/src/Basiclight.cpp" "/Users/Sean/Documents/CSCI5607/Assignment1b/build/CMakeFiles/imgmaker.dir/src/Basiclight.cpp.o"
+  "/Users/Sean/Documents/CSCI5607/Assignment1b/src/Camera.cpp" "/Users/Sean/Documents/CSCI5607/Assignment1b/build/CMakeFiles/imgmaker.dir/src/Camera.cpp.o"
+  "/Users/Sean/Documents/CSCI5607/Assignment1b/src/Ellipsoid.cpp" "/Users/Sean/Documents/CSCI5607/Assignment1b/build/CMakeFiles/imgmaker.dir/src/Ellipsoid.cpp.o"
+  "/Users/Sean/Documents/CSCI5607/Assignment1b/src/Entity.cpp" "/Users/Sean/Documents/CSCI5607/Assignment1b/build/CMakeFiles/imgmaker.dir/src/Entity.cpp.o"
+  "/Users/Sean/Documents/CSCI5607/Assignment1b/src/Image.cpp" "/Users/Sean/Documents/CSCI5607/Assignment1b/build/CMakeFiles/imgmaker.dir/src/Image.cpp.o"
+  "/Users/Sean/Documents/CSCI5607/Assignment1b/src/ImgSettings.cpp" "/Users/Sean/Documents/CSCI5607/Assignment1b/build/CMakeFiles/imgmaker.dir/src/ImgSettings.cpp.o"
+  "/Users/Sean/Documents/CSCI5607/Assignment1b/src/Light.cpp" "/Users/Sean/Documents/CSCI5607/Assignment1b/build/CMakeFiles/imgmaker.dir/src/Light.cpp.o"
+  "/Users/Sean/Documents/CSCI5607/Assignment1b/src/Material.cpp" "/Users/Sean/Documents/CSCI5607/Assignment1b/build/CMakeFiles/imgmaker.dir/src/Material.cpp.o"
+  "/Users/Sean/Documents/CSCI5607/Assignment1b/src/Object.cpp" "/Users/Sean/Documents/CSCI5607/Assignment1b/build/CMakeFiles/imgmaker.dir/src/Object.cpp.o"
+  "/Users/Sean/Documents/CSCI5607/Assignment1b/src/Phong.cpp" "/Users/Sean/Documents/CSCI5607/Assignment1b/build/CMakeFiles/imgmaker.dir/src/Phong.cpp.o"
+  "/Users/Sean/Documents/CSCI5607/Assignment1b/src/Sphere.cpp" "/Users/Sean/Documents/CSCI5607/Assignment1b/build/CMakeFiles/imgmaker.dir/src/Sphere.cpp.o"
+  "/Users/Sean/Documents/CSCI5607/Assignment1b/src/Spotlight.cpp" "/Users/Sean/Documents/CSCI5607/Assignment1b/build/CMakeFiles/imgmaker.dir/src/Spotlight.cpp.o"
+  "/Users/Sean/Documents/CSCI5607/Assignment1b/src/Vec3.cpp" "/Users/Sean/Documents/CSCI5607/Assignment1b/build/CMakeFiles/imgmaker.dir/src/Vec3.cpp.o"
+  "/Users/Sean/Documents/CSCI5607/Assignment1b/src/main.cpp" "/Users/Sean/Documents/CSCI5607/Assignment1b/build/CMakeFiles/imgmaker.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
