7386b2cf5c38dd296d92dd9be710e0d0b5618db1
Sean Lin <linx0486@umn.edu>
2016-10-11 09:37:45 -0500

    add polygons

---
+++ Assignment1c/build/CMakeFiles/imgmaker.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/Sean/Documents/CSCI5607/Assignment1c/src/Basiclight.cpp" "/Users/Sean/Documents/CSCI5607/Assignment1c/build/CMakeFiles/imgmaker.dir/src/Basiclight.cpp.o"
+  "/Users/Sean/Documents/CSCI5607/Assignment1c/src/Camera.cpp" "/Users/Sean/Documents/CSCI5607/Assignment1c/build/CMakeFiles/imgmaker.dir/src/Camera.cpp.o"
+  "/Users/Sean/Documents/CSCI5607/Assignment1c/src/Ellipsoid.cpp" "/Users/Sean/Documents/CSCI5607/Assignment1c/build/CMakeFiles/imgmaker.dir/src/Ellipsoid.cpp.o"
+  "/Users/Sean/Documents/CSCI5607/Assignment1c/src/Entity.cpp" "/Users/Sean/Documents/CSCI5607/Assignment1c/build/CMakeFiles/imgmaker.dir/src/Entity.cpp.o"
+  "/Users/Sean/Documents/CSCI5607/Assignment1c/src/Image.cpp" "/Users/Sean/Documents/CSCI5607/Assignment1c/build/CMakeFiles/imgmaker.dir/src/Image.cpp.o"
+  "/Users/Sean/Documents/CSCI5607/Assignment1c/src/ImgSettings.cpp" "/Users/Sean/Documents/CSCI5607/Assignment1c/build/CMakeFiles/imgmaker.dir/src/ImgSettings.cpp.o"
+  "/Users/Sean/Documents/CSCI5607/Assignment1c/src/Light.cpp" "/Users/Sean/Documents/CSCI5607/Assignment1c/build/CMakeFiles/imgmaker.dir/src/Light.cpp.o"
+  "/Users/Sean/Documents/CSCI5607/Assignment1c/src/Material.cpp" "/Users/Sean/Documents/CSCI5607/Assignment1c/build/CMakeFiles/imgmaker.dir/src/Material.cpp.o"
+  "/Users/Sean/Documents/CSCI5607/Assignment1c/src/Normal.cpp" "/Users/Sean/Documents/CSCI5607/Assignment1c/build/CMakeFiles/imgmaker.dir/src/Normal.cpp.o"
+  "/Users/Sean/Documents/CSCI5607/Assignment1c/src/Object.cpp" "/Users/Sean/Documents/CSCI5607/Assignment1c/build/CMakeFiles/imgmaker.dir/src/Object.cpp.o"
+  "/Users/Sean/Documents/CSCI5607/Assignment1c/src/Phong.cpp" "/Users/Sean/Documents/CSCI5607/Assignment1c/build/CMakeFiles/imgmaker.dir/src/Phong.cpp.o"
+  "/Users/Sean/Documents/CSCI5607/Assignment1c/src/Polygon.cpp" "/Users/Sean/Documents/CSCI5607/Assignment1c/build/CMakeFiles/imgmaker.dir/src/Polygon.cpp.o"
+  "/Users/Sean/Documents/CSCI5607/Assignment1c/src/Sphere.cpp" "/Users/Sean/Documents/CSCI5607/Assignment1c/build/CMakeFiles/imgmaker.dir/src/Sphere.cpp.o"
+  "/Users/Sean/Documents/CSCI5607/Assignment1c/src/Spotlight.cpp" "/Users/Sean/Documents/CSCI5607/Assignment1c/build/CMakeFiles/imgmaker.dir/src/Spotlight.cpp.o"
+  "/Users/Sean/Documents/CSCI5607/Assignment1c/src/Triangle.cpp" "/Users/Sean/Documents/CSCI5607/Assignment1c/build/CMakeFiles/imgmaker.dir/src/Triangle.cpp.o"
+  "/Users/Sean/Documents/CSCI5607/Assignment1c/src/UvCoord.cpp" "/Users/Sean/Documents/CSCI5607/Assignment1c/build/CMakeFiles/imgmaker.dir/src/UvCoord.cpp.o"
+  "/Users/Sean/Documents/CSCI5607/Assignment1c/src/Vec3.cpp" "/Users/Sean/Documents/CSCI5607/Assignment1c/build/CMakeFiles/imgmaker.dir/src/Vec3.cpp.o"
+  "/Users/Sean/Documents/CSCI5607/Assignment1c/src/Vertex.cpp" "/Users/Sean/Documents/CSCI5607/Assignment1c/build/CMakeFiles/imgmaker.dir/src/Vertex.cpp.o"
+  "/Users/Sean/Documents/CSCI5607/Assignment1c/src/main.cpp" "/Users/Sean/Documents/CSCI5607/Assignment1c/build/CMakeFiles/imgmaker.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ Assignment1c/build/CMakeFiles/imgmaker.dir/cmake_clean.cmake
@@ -0,0 +1,28 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/imgmaker.dir/src/main.cpp.o"
+  "CMakeFiles/imgmaker.dir/src/ImgSettings.cpp.o"
+  "CMakeFiles/imgmaker.dir/src/Vec3.cpp.o"
+  "CMakeFiles/imgmaker.dir/src/Camera.cpp.o"
+  "CMakeFiles/imgmaker.dir/src/Image.cpp.o"
+  "CMakeFiles/imgmaker.dir/src/Object.cpp.o"
+  "CMakeFiles/imgmaker.dir/src/Sphere.cpp.o"
+  "CMakeFiles/imgmaker.dir/src/Ellipsoid.cpp.o"
+  "CMakeFiles/imgmaker.dir/src/Material.cpp.o"
+  "CMakeFiles/imgmaker.dir/src/Phong.cpp.o"
+  "CMakeFiles/imgmaker.dir/src/Light.cpp.o"
+  "CMakeFiles/imgmaker.dir/src/Spotlight.cpp.o"
+  "CMakeFiles/imgmaker.dir/src/Basiclight.cpp.o"
+  "CMakeFiles/imgmaker.dir/src/Entity.cpp.o"
+  "CMakeFiles/imgmaker.dir/src/Vertex.cpp.o"
+  "CMakeFiles/imgmaker.dir/src/Polygon.cpp.o"
+  "CMakeFiles/imgmaker.dir/src/Triangle.cpp.o"
+  "CMakeFiles/imgmaker.dir/src/UvCoord.cpp.o"
+  "CMakeFiles/imgmaker.dir/src/Normal.cpp.o"
+  "imgmaker.pdb"
+  "imgmaker"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/imgmaker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
